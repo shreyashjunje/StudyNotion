11dfaef401ff9f2bdd09ae324ffa0471077819db
shreyashjunje <shreyashjunje2003@gmail.com>
2024-06-25 23:47:18 +0530

    started frontend for this project

---
+++ src/components/core/Homepage/CodeBlocks.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CTAButton from "../Homepage/Button"
+import HighlightText from './HighlightText'
+import { FaArrowRight } from "react-icons/fa";
+
+
+const CodeBlocks = ({
+    position,heading,subHeading,ctabtn1,ctabtn2,codeblack,backgroundGradient,codeColor
+}) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CodeBlocks
