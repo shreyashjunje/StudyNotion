bcce88c277af4130577797a709eb4e957f795e5d
shreyashjunje <shreyashjunje2003@gmail.com>
2024-06-28 23:37:47 +0530

    worked on navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Route,Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import AboutUs from "./pages/AboutUs";
+import Navbar from "./components/common/Navbar";
 function App() {
   return (
     <div className="  w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+        <Navbar/>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/signup" element={<SignUp/>}/>
