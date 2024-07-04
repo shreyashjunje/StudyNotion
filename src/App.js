ad3a6bb44993da4effc34e9192069e61c893de69
shreyashjunje <shreyashjunje2003@gmail.com>
2024-07-04 13:17:13 +0530

    fix: build errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import AboutUs from "./pages/AboutUs";
 import Navbar from "./components/common/Navbar";
 function App() {
   return (
-    <div className="  w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-            <Route path="/aboutus" element={<AboutUs/>}/>
-        </Routes>
+    <div className="w-full min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
--- src/components/core/AboutUspage/OurVisionAndMission.jsx
+++ src/components/core/AboutUspage/OurVisionAndMission.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const OurVisionAndMission = () => {
-  return ( 
-    <div className='flex  items-center justify-center  h-full w-[100%]  gap-1'>
-
-        <div className='flex flex-col w-[50%] gap-8'>
-
-            <h2 className='gradient-text-light-orange text-4xl font-bold'>Our Vision</h2>
-            <div className='flex flex-col text-start text-richblack-600 w-[60%]'>
-                 With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.
-            </div>
-
+  return (
+    <div className="flex mx-auto items-center justify-center h-full w-[100%] ">
+      <div className="flex flex-col  w-[40%] gap-8">
+        <h2 className="gradient-text-light-orange text-4xl font-bold">
+          Our Vision
+        </h2>
+        <div className="flex flex-col text-start text-richblack-600 w-[60%]">
+          With this vision in mind, we set out on a journey to create an
+          e-learning platform that would revolutionize the way people learn. Our
+          team of dedicated experts worked tirelessly to develop a robust and
+          intuitive platform that combines cutting-edge technology with engaging
+          content, fostering a dynamic and interactive learning experience.
         </div>
+      </div>
 
-        <div className='flex flex-col gap-8 w-[50%]'>
-
-            <h2 className='gradient-text-blue text-4xl font-bold'>Our Mission</h2>
-            <div className='flex flex-col text-start text-richblack-600 w-[60%]'>
-                our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.
-            </div>
-
+      <div className="flex flex-col  gap-8 w-[40%]">
+        <h2 className="gradient-text-blue text-4xl font-bold">Our Mission</h2>
+        <div className="flex flex-col text-start text-richblack-600 w-[60%]">
+          our mission goes beyond just delivering courses online. We wanted to
+          create a vibrant community of learners, where individuals can connect,
+          collaborate, and learn from one another. We believe that knowledge
+          thrives in an environment of sharing and dialogue, and we foster this
+          spirit of collaboration through forums, live sessions, and networking
+          opportunities.
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OurVisionAndMission
+export default OurVisionAndMission;
