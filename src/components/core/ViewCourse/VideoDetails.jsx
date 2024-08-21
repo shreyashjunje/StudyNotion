fc8ba049d7a9fe96d2772f90cbe589a388d17a23
shreyashjunje <shreyashjunje2003@gmail.com>
2024-08-21 23:17:21 +0530

    updated videoDetails page

--- src/components/core/ViewCourse/VideoDetails.jsx
+++ src/components/core/ViewCourse/VideoDetails.jsx
@@ -41,16 +41,75 @@ const VideoDetails = () => {
 
   const isFirstVideo=()=>{
 
+    const currentSectionIndex=courseSectionData.findIndex(
+      (data)=>data._id === sectionId
+    )
+
+    const currentSubSectionIndex=courseSectionData[currentSectionIndex].subSectionId.findIndex(
+      (data)=>data._id === subSectionId
+    )
+
+    if(currentSubSectionIndex === 0 && currentSubSectionIndex===0){
+      return true;
+    }else{
+      return false
+    }
+
+
+
   }
   const isLastVideo=()=>{
+
+    const currentSectionIndex=courseSectionData.findIndex(
+      (data)=>data._id === sectionId
+    )
+
+    const noOfSubSections=courseSectionData[curretnSectionIndex].subSection.length;
+
+    const currentSubSectionIndex=courseSectionData[currentSectionIndex].subSectionId.findIndex(
+      (data)=>data._id === subSectionId
+    )
+
+    if(currentSectionIndex===courseSectionData.length-1 && 
+      currentSubSectionIndex === noOfSubSections-1
+    ){
+      return true
+    }else{
+      return false
+    }
 
   }
   
   const goToNext=()=>{
 
+    const currentSectionIndex=courseSectionData.findIndex(
+      (data)=>data._id === sectionId
+    )
+
+    const noOfSubSections=courseSectionData[curretnSectionIndex].subSection.length;
+
+    const currentSubSectionIndex=courseSectionData[currentSectionIndex].subSectionId.findIndex(
+      (data)=>data._id === subSectionId
+    )
+
+    if(currentSubSectionIndex!==noOfSubSections-1){
+        // same section ki next video pe aa jao
+        const nextSubSectionId=courseSectionData[currentSectionIndex].subSection[currentSectionIndex + 1]._id
+
+        // is video pr jao
+        navigate(`/view-course/${courseId}/section/${sectionId}/sub-section/${nextSubSectionId}`)
+    }else{
+      // different setion ki first video
+      const nextSectionId=courseSectionData[currentSectionIndex + 1]
+      const nextSubSectionId=courseSectionData[currentSectionIndex + 1].subSection[0]._id;
+      navigate(`/view-course/${courseId}/section/${nextSectionId}/sub-section/${nextSubSectionId}`)
+
+    }
+
   }
 
   const goToPrevVideo=()=>{
+    
 
   }
 
